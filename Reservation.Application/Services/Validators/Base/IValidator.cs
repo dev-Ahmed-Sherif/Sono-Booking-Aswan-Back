a6ac111174ac92480656a983d13f21b26ac28138
Ahmed Sherif <ahmedshiref911@gmail.com>
2026-05-02 01:05:18 +0300

    remove old application 2 5 2026

--- Reservation.Application/Services/Validators/Base/IValidator.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace SonoTracker.Application.Services.Validators.Base
-{
-    public interface IValidator<in T> where T : class
-    {
-        Task<(bool, string)> Validate(T entity);
-    }
-}
